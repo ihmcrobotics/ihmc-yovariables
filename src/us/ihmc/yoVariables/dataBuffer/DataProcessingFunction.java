cddcc3c5a97320b2a9d5d39fef986baa405cc014
Nathan Mertins <nmertins@ihmc.us>
2017-06-14 18:55:32 -0500

    moved DataProcessingFunction to IHMCYoVariables

---
+++ src/us/ihmc/yoVariables/dataBuffer/DataProcessingFunction.java
@@ -0,0 +1,18 @@
+package us.ihmc.yoVariables.dataBuffer;
+
+
+/**
+ *
+ * <p>DataProcessingFunction</p>
+ *
+ * <p>A function to be applied to the recorded simulation data from the in point to the out point.</p>
+ *
+ */
+public interface DataProcessingFunction
+{
+   public void initializeProcessing();
+   /**
+    * Defines the function to be applied to the data.
+    */
+   public void processData();
+}
