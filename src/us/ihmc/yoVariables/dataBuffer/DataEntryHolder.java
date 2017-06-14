f6086a8d1a727747af938b9c2c3266f7615a21e3
Nathan Mertins <nmertins@ihmc.us>
2017-06-14 15:20:26 -0500

    moved DataEntry, DataEntryHolder, and TimeDataHolder to IHMCYoVariables repo

---
+++ src/us/ihmc/yoVariables/dataBuffer/DataEntryHolder.java
@@ -0,0 +1,8 @@
+package us.ihmc.yoVariables.dataBuffer;
+
+import us.ihmc.yoVariables.variable.YoVariable;
+
+public interface DataEntryHolder
+{
+   public DataEntry getEntry(YoVariable<?> yoVariable);
+}
