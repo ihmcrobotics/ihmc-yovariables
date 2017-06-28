9a13834b04363535b74ed4dc3498ecbc23773a5a
Nathan Mertins <nmertins@ihmc.us>
2017-06-28 10:21:21 -0500

    created test suite for running all the tests with coverage

---
+++ test/us/ihmc/yoVariables/IHMCYoVariableTestSuite.java
@@ -0,0 +1,28 @@
+package us.ihmc.yoVariables;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import us.ihmc.yoVariables.dataBuffer.DataBufferEntryTest;
+import us.ihmc.yoVariables.dataBuffer.DataBufferTest;
+import us.ihmc.yoVariables.registry.NameSpaceTest;
+import us.ihmc.yoVariables.registry.YoVariableRegistryTest;
+import us.ihmc.yoVariables.variable.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+      DataBufferEntryTest.class,
+      DataBufferTest.class,
+      NameSpaceTest.class,
+      YoVariableRegistryTest.class,
+      YoBooleanTest.class,
+      YoDoubleTest.class,
+      YoEnumTest.class,
+      YoIntegerTest.class,
+      YoVariableListTest.class,
+      YoVariableTest.class,
+      YoVariableHolderImplementationTest.class,
+      YoVariableHolderImplementationNewTest.class
+})
+public class IHMCYoVariableTestSuite
+{
+}
