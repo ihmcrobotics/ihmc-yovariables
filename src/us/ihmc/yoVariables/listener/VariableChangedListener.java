7182ea62159b08687562f58bd2eaa9e27b234fb9
Nathan Mertins <nmertins@ihmc.us>
2017-07-14 11:32:42 -0500

    javadoc for listener interfaces

--- src/us/ihmc/yoVariables/listener/VariableChangedListener.java
+++ src/us/ihmc/yoVariables/listener/VariableChangedListener.java
@@ -3,10 +3,16 @@ package us.ihmc.yoVariables.listener;
 import us.ihmc.yoVariables.variable.YoVariable;
 
 /**
+ * Listener on the backing value of a {@link YoVariable}.
+ *
  * @author Jerry Pratt
  */
-
 public interface VariableChangedListener
 {
+   /**
+    * Called when the primitive data-type backing a YoVariable is changed
+    *
+    * @param v YoVariable the listener is attached to
+    */
    public void variableChanged(YoVariable<?> v);
 }
