171bf2b2849c74a81504dff5837b34726a531293
Carson Wilber <cwilber@ihmc.us>
2017-08-01 10:21:08 -0500

    Almost completed YoInteger javadoc.

--- src/us/ihmc/yoVariables/variable/YoInteger.java
+++ src/us/ihmc/yoVariables/variable/YoInteger.java
@@ -2,6 +2,9 @@ package us.ihmc.yoVariables.variable;
 
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
+/**
+ * Integer implementation of the YoVariable class.
+ */
 public class YoInteger extends YoVariable<YoInteger>
 {
    private int val;
@@ -19,6 +22,14 @@ public class YoInteger extends YoVariable<YoInteger>
       this.manualMaxScaling = maxScaling;
    }
 
+   /**
+    * Create a new YoInteger. This will call its super YoVariable's {@link YoVariable(YoVariableType, String, String, YoVariableRegistry)}
+    * with YoVariableType.INTEGER and the given values.
+    *
+    * @param name 
+    * @param description
+    * @param registry
+    */
    public YoInteger(String name, String description, YoVariableRegistry registry)
    {
       super(YoVariableType.INTEGER, name, description, registry);
@@ -27,23 +38,28 @@ public class YoInteger extends YoVariable<YoInteger>
    }
 
    /**
-    * Set the value of this YoVariable.  All four types are represented via the same interal double.  If
-    * of integer type this value is cast as an int whenever accessed.  It represents the ordinal if of enum type.
-    * Boolean values are triggered around the 0.5 threshold with < 0.5 being false and greater or equal being true.
+    * Calls {@link #set(int, boolean)} with (value, true).
     *
-    * @param value double value to store
+    * @param value to set this YoInteger's internal integer state to
     */
    public void set(int value)
    {
       set(value, true);
    }
-   
+
+   /**
+    * Sets this YoInteger to the given value.
+    *
+    * @param value to set this YoInteger's internal integer state to
+    * @param notifyListeners boolean determining whether or not to call {@link #notifyVariableChangedListeners()}
+    * @return boolean if the given value differed from the current value of this YoInteger
+    */
    public boolean set(int value, boolean notifyListeners)
    {
       if (val != value)
       {
          val = value;
-         if(notifyListeners)
+         if (notifyListeners)
          {
             notifyVariableChangedListeners();
          }
@@ -51,130 +67,181 @@ public class YoInteger extends YoVariable<YoInteger>
       }
       return false;
    }
-   
+
+   /**
+    * Sets this YoInteger to its current value plus one.
+    */
    public void increment()
    {
-      this.set(this.getIntegerValue()+1);
+      this.set(this.getIntegerValue() + 1);
    }
-   
+
+   /**
+    * Sets this YoInteger to its current value minus one.
+    */
    public void decrement()
    {
-      this.set(this.getIntegerValue()-1);
+      this.set(this.getIntegerValue() - 1);
    }
-   
+
+   /**
+    * Sets this YoInteger to its current value plus the given value.
+    *
+    * @param value to add to this YoInteger
+    */
    public void add(int value)
    {
       this.set(this.getIntegerValue() + value);
    }
-   
+
+   /**
+    * Sets this YoInteger to its current value minus the given value.
+    *
+    * @param value to subtract from this YoInteger
+    */
    public void subtract(int value)
    {
       this.set(this.getIntegerValue() - value);
    }
 
    /**
-    * Retrieve the integer representation of this variable.  If not of integer type a warning will be printed.
+    * Retrieves the value of this YoInteger.
     *
-    * @return int value of this
+    * @return the internal integer state of this YoInteger
     */
    public int getIntegerValue()
    {
-//      if (val != Math.round(val))
-//         System.err.println("YoInteger: returning a different value than what it used to be.");
       return val;
    }
 
    /**
-    * Check if the value contained by this variable is equal to the given integer.  If not of integer type
-    * a warning will be printed to the console.
+    * Check if the value contained by this YoInteger is equal to the given integer.
     *
-    * @param value int to be compared
-    * @return boolean are they equal?
+    * @param value int for this YoInteger to be compared to
+    * @return boolean if this YoInteger's value is the same as the passed value
     */
    public boolean valueEquals(int value)
    {
       return val == value;
    }
 
-   @Override
-   public void setValueFromDouble(double doubleValue, boolean notifyListeners)
+   /**
+    * Set the value of this YoInteger using the given double, passed through {@link #convertFromDoubleToInt(double)}.
+    *
+    * @param doubleValue to convert and set this YoInteger to
+    * @param notifyListeners boolean determining whether or not to call {@link #notifyVariableChangedListeners()}
+    */
+   @Override public void setValueFromDouble(double doubleValue, boolean notifyListeners)
    {
       set(convertFromDoubleToInt(doubleValue), notifyListeners);
    }
-   
+
+   /**
+    * Returns the given double value converted to the YoInteger representation.
+    *
+    * @param doubleValue double to convert
+    * @return rounded integer value representing the double
+    */
    public int convertFromDoubleToInt(double doubleValue)
    {
       // Note: do not expect this to work well for very large values!
       return (int) Math.round(doubleValue);
-      //   return (new Double(doubleValue).intValue());
-   }
-
-   @Override
-   public double getValueAsDouble()
-   {
-      return val;
-
-   }
-
-   @Override
-   public String toString()
-   {
-      StringBuffer retBuffer = new StringBuffer();
-      retBuffer.append(getName());
-      retBuffer.append(": ");
-      retBuffer.append(getIntegerValue());
-      return retBuffer.toString();
    }
 
    /**
-    * Appends the value of this variable to the end of the given StringBuffer.  This representation is
-    * based on variable type.
+    * Retrieves this YoInteger's value as a double.
+    *
+    * @return return-casted double value of this YoInteger's internal integer value
+    */
+   @Override public double getValueAsDouble()
+   {
+      return val;
+   }
+
+   /**
+    * Returns String representation of this YoInteger.
+    *
+    * @return a String representing this YoInteger and its current value as an integer
+    */
+   @Override public String toString()
+   {
+      return String.format("%s: %d", getName(), getIntegerValue());
+   }
+
+   /**
+    * Appends the value of this YoInteger to the end of the given StringBuffer.
     *
     * @param stringBuffer StringBuffer to which the value will be appended
     */
-   @Override
-   public void getValueString(StringBuffer stringBuffer)
+   @Override public void getValueString(StringBuffer stringBuffer)
    {
       stringBuffer.append(val);
-
    }
-   
-   @Override
-   public void getValueStringFromDouble(StringBuffer stringBuffer, double doubleValue)
+
+   /**
+    * Appends the YoInteger representation of the given double value to the given StringBuffer.
+    *
+    * @param stringBuffer StringBuffer to append to
+    * @param doubleValue double value to convert to YoInteger representation
+    */
+   @Override public void getValueStringFromDouble(StringBuffer stringBuffer, double doubleValue)
    {
       stringBuffer.append(convertFromDoubleToInt(doubleValue));
    }
 
-   @Override
-   public long getValueAsLongBits()
+   /**
+    * Retrieves this YoInteger's value as a long.
+    *
+    * @return return-casted long value of this YoInteger's internal integer value
+    */
+   @Override public long getValueAsLongBits()
    {
       return val;
    }
 
-   @Override
-   public void setValueFromLongBits(long value, boolean notifyListeners)
+   /**
+    * Sets the internal integer value of this YoInteger using the static integer cast of the passed long value.
+    *
+    * @param value long to set this variable's value to
+    * @param notifyListeners boolean determining whether or not to call {@link #notifyVariableChangedListeners()}
+    */
+   @Override public void setValueFromLongBits(long value, boolean notifyListeners)
    {
       set((int) value, notifyListeners);
    }
 
-   @Override
-   public YoInteger duplicate(YoVariableRegistry newRegistry)
+   /**
+    * Creates a new YoInteger with the same parameters as this one, and registers it to the passed {@link YoVariableRegistry}.
+    *
+    * @param newRegistry YoVariableRegistry to duplicate this variable to
+    * @return the newly created and registered YoInteger
+    */
+   @Override public YoInteger duplicate(YoVariableRegistry newRegistry)
    {
       YoInteger retVar = new YoInteger(getName(), getDescription(), newRegistry, getManualScalingMin(), getManualScalingMax());
       retVar.set(getIntegerValue());
       return retVar;
    }
 
-   @Override
-   public boolean setValue(YoInteger value, boolean notifyListeners)
+   /**
+    * Sets the interal value of this YoInteger to the current value of the passed YoInteger.
+    *
+    * @param value YoInteger value to set this variable's value to
+    * @param notifyListeners boolean determining whether or not to call {@link #notifyVariableChangedListeners()}
+    * @return boolean whether or not internal state differed from the passed value
+    */
+   @Override public boolean setValue(YoInteger value, boolean notifyListeners)
    {
       return set(value.getIntegerValue(), notifyListeners);
    }
 
-   @Override
-   public boolean isZero()
+   /**
+    * Assesses if this YoInteger is equal to zero.
+    *
+    * @return boolean if this YoInteger's internal integer value is equal to integer 0
+    */
+   @Override public boolean isZero()
    {
       return getIntegerValue() == 0;
    }
-
 }
