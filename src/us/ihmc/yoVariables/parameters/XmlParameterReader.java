a81b1b092416a94f9c026f684880bb7861dccb3b
Jesper Smith <jsmith@ihmc.us>
2017-08-21 16:12:00 -0500

    Added parameter reader

--- src/us/ihmc/yoVariables/parameters/XmlParameterReader.java
+++ src/us/ihmc/yoVariables/parameters/XmlParameterReader.java
@@ -15,42 +15,93 @@
  */
 package us.ihmc.yoVariables.parameters;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.util.HashMap;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.io.input.ReaderInputStream;
+
+import us.ihmc.yoVariables.parameters.xml.Parameter;
+import us.ihmc.yoVariables.parameters.xml.Parameters;
+import us.ihmc.yoVariables.parameters.xml.Registry;
 import us.ihmc.yoVariables.registry.NameSpace;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 public class XmlParameterReader extends AbstractParameterReader
 {
-   public XmlParameterReader(InputStream data)
+   private final HashMap<String, String> parameterValues = new HashMap<>();
+
+   public XmlParameterReader(InputStream data) throws IOException
    {
-      
+      try
+      {
+         JAXBContext jaxbContext = JAXBContext.newInstance(Parameters.class);
+         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+         Parameters parameterRoot = (Parameters) jaxbUnmarshaller.unmarshal(data);
+         for(Registry registry : parameterRoot.getRegistries())
+         {
+            addRegistry(registry.getName(), registry);
+         }
+      }
+      catch (JAXBException e)
+      {
+         throw new IOException(e);
+      }
+
    }
-   
-   
-   
+
+   private void addRegistry(String path, Registry registry)
+   {
+      if(registry.getParameters() != null)
+      {
+         for(Parameter param : registry.getParameters())
+         {
+            String name = path + "." + param.getName();
+            parameterValues.put(name, param.getValue());
+            System.out.println("Adding " + name + " value " + param.getValue());
+         }
+      }
+      
+      if(registry.getRegistries() != null)
+      {
+         for(Registry child : registry.getRegistries())
+         {
+            String childPath = path + "." + child.getName();
+            addRegistry(childPath, child);
+         }         
+      }
+   }
+
    @Override
    protected boolean hasValue(NameSpace namespace, String name)
    {
-      // TODO Auto-generated method stub
-      return false;
+      String fullname = namespace.getName() + "." + name;
+      return parameterValues.containsKey(fullname);
    }
-
-
-
 
    @Override
    protected String getValue(NameSpace namespace, String name)
    {
-      // TODO Auto-generated method stub
-      return null;
+      String fullname = namespace.getName() + "." + name;
+      System.out.println("Requesting " + fullname + " value " + parameterValues.get(fullname));
+      return parameterValues.get(fullname);
    }
-   
 
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-      
+      String data = "<parameters>    <registry name=\"root\">        <registry name=\"a\">            <registry name=\"b\">                <registry name=\"c\">                    <parameter name=\"paramA\" type=\"DoubleParameter\" value=\"6.0\"/>                    <parameter name=\"paramB\" type=\"DoubleParameter\" value=\"0.0\"/>                    <parameter name=\"paramC\" type=\"DoubleParameter\" value=\"0.0\"/>                    <parameter name=\"paramD\" type=\"DoubleParameter\" value=\"0.0\"/>                </registry>                <parameter name=\"paramE\" type=\"DoubleParameter\" value=\"0.0\"/>            </registry>            <parameter name=\"paramF\" type=\"DoubleParameter\" value=\"0.0\"/>        </registry>        <parameter name=\"paramG\" type=\"DoubleParameter\" value=\"0.0\"/>    </registry></parameters>";
+
+      XmlParameterReader reader = new XmlParameterReader(new ReaderInputStream(new StringReader(data)));
+
+      YoVariableRegistry regs = XmlParameterWriter.createRegistries();
+
+      reader.readParametersInRegistry(regs);
    }
-
-
 
 }
