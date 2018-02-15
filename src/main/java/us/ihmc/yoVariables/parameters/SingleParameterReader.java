d4c6ebc7067649d2a78c6e43249f3f8e10855b4f
gwiedebach <gwiedebach@ihmc.us>
2018-02-15 14:45:45 -0800

    Added a parameter reader that can be used to load a parameter and set its load status.

---
+++ src/main/java/us/ihmc/yoVariables/parameters/SingleParameterReader.java
@@ -0,0 +1,14 @@
+package us.ihmc.yoVariables.parameters;
+
+public class SingleParameterReader
+{
+   public static void readParameter(YoParameter<?> parameter, double doubleValue, ParameterLoadStatus loadStatus)
+   {
+      if (loadStatus == ParameterLoadStatus.UNLOADED)
+      {
+         throw new RuntimeException("Can not load parameter and set the status to unloaded.");
+      }
+      parameter.getVariable().setValueFromDouble(doubleValue);
+      parameter.loadStatus = loadStatus;
+   }
+}
