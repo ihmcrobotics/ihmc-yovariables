6dc33e475d3c8f75d158578d67ba55f3b1e91087
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-30 15:33:41 -0500

    :arrow_up: euclid 0.14.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,10 @@ dependencies {
    api("org.glassfish.jaxb:jaxb-runtime:2.3.2")
 
    api("us.ihmc:ihmc-commons:0.28.2")
-   api("us.ihmc:euclid-frame:0.13.1")
+   api("us.ihmc:euclid-frame:0.14.0")
 }
 
 testDependencies {
    api("us.ihmc:ihmc-commons-testing:0.28.2")
-   api("us.ihmc:euclid-test:0.13.0")
+   api("us.ihmc:euclid-test:0.14.0")
 }
