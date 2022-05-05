3ca0bb5cd181105e370c5582b82eda9abc8da2a6
Duncan Calvert <dcalvert@ihmc.us>
2022-05-04 19:13:07 -0500

    :arrow_up: ihmc-commons 0.30.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,11 @@ dependencies {
    api("jakarta.xml.bind:jakarta.xml.bind-api:2.3.2")
    api("org.glassfish.jaxb:jaxb-runtime:2.3.2")
 
-   api("us.ihmc:ihmc-commons:0.30.5")
+   api("us.ihmc:ihmc-commons:0.30.6")
    api("us.ihmc:euclid-frame:0.17.2")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.30.5")
+   api("us.ihmc:ihmc-commons-testing:0.30.6")
    api("us.ihmc:euclid-test:0.17.0")
 }
