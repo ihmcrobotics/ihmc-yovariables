fa9c91ae0532c5c12e1d3f4d7a7a4f8d14794b07
Sylvain Bertrand <sbertrand@ihmc.org>
2023-10-09 13:20:25 -0500

    :bookmark: 0.9.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.9.18"
+   version = "0.9.19"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-yovariables"
    openSource = true
    maintainer = "Sylvain Bertrand (sbertrand@ihmc.us)"
