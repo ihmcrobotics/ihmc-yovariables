15dba0396643cde20dea6d9f455b8911e3c88f02
James Foster <jfoster@ihmc.org>
2024-05-20 09:36:33 -0500

    :bookmark: 0.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.11.1"
+   version = "0.12.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-yovariables"
    openSource = true
    maintainer = "Sylvain Bertrand (sbertrand@ihmc.us)"
