cb64235816be696f170eb9d687a2798c351ce7a9
Duncan Calvert <dcalvert@ihmc.us>
2024-08-09 16:18:42 -0500

    Remove unnecessary jaxb dependencies. (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ ihmc {
 
 dependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("com.sun.xml.bind:jaxb-ri:4.0.5")
+   api("com.sun.xml.bind:jaxb-impl:4.0.5")
 
    api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:euclid-frame:0.21.0")
