c7d39927f85e25a26bb51d271b317cb3f6f334ab
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-24 08:41:59 -0500

    :arrow_up: ihmc-commons 0.33.0
    :arrow_up: euclid 0.22.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,12 @@ dependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("com.sun.xml.bind:jaxb-impl:4.0.5")
 
-   api("us.ihmc:ihmc-commons:0.32.0")
-   api("us.ihmc:euclid-frame:0.21.0")
-//   api("org.ejml:ejml-ddense:0.39")
+   api("us.ihmc:ihmc-commons:0.33.0")
+   api("us.ihmc:euclid-frame:0.22.0")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.32.0")
-   api("us.ihmc:euclid-test:0.21.0")
+   api("us.ihmc:ihmc-commons-testing:0.33.0")
+   api("us.ihmc:euclid-test:0.22.0")
    api("org.apache.commons:commons-math3:3.3")
 }
