c46bc60a08b39b62bfecf7dc7fb0bc722a69e430
Robert <rjgriffin42@gmail.com>
2026-02-02 16:00:59 -0600

    :bookmark: 0.13.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.13.6"
+   version = "0.13.7"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-yovariables"
    openSource = true
    maintainer = "Robert Griffin (rgriffin@ihmc.org)"
