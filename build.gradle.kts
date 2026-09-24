40e6fbad7baa235d3de3bc1072d42250f7aaa74c
nkitchel <nkitchel@ihmc.org>
2026-09-24 09:58:38 -0500

    :bookmark: 0.13.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.13.7"
+   version = "0.13.8"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-yovariables"
    openSource = true
    maintainer = "Robert Griffin (rgriffin@ihmc.org)"
